ba0fb88fb1160404a7354efc9bd9463a38581341
mriusero <160402415+mriusero@users.noreply.github.com>
2025-01-27 12:33:44 +0100

    feat: add streamlit app template

---
+++ src/front-end/app.py
@@ -0,0 +1,8 @@
+from app.layout import app_layout
+
+def main():
+
+    app_layout()
+
+if __name__ == '__main__':
+    main()
---
+++ src/front-end/app/layouts/page5.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def page_5():
+    st.markdown('<div class="header">#5 Empty page_</div>', unsafe_allow_html=True)
