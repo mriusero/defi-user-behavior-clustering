1c879daca261c746c8f0a9127cc09326f1ba6411
mriusero <160402415+mriusero@users.noreply.github.com>
2025-01-28 09:31:08 +0100

    fix: pylint

--- src/backend/db/mongodb_handler.py
+++ src/backend/db/mongodb_handler.py
@@ -6,20 +6,17 @@ from pymongo import MongoClient
 def get_mongo_client(mongo_uri="mongodb://localhost:27017/"):
     """
     Connects to a MongoDB server and returns the client object.
-    :param:
-        mongo_uri (str): URI of the MongoDB server. Default is 'mongodb://localhost:27017'.
-    :return:
-        MongoClient: The MongoDB client object.
-    :raise:
-        ValueError: If the URI is invalid.
+    :param: mongo_uri (str): URI of the MongoDB server. Default is 'mongodb://localhost:27017'.
+    :return: MongoClient: The MongoDB client object.
+    :raise: ValueError: If the URI is invalid.
     """
     try:
         client = MongoClient(mongo_uri)
-        logging.info(f"Connected to MongoDB at {mongo_uri}")
+        logging.info("Connected to MongoDB at %s", mongo_uri)
         return client
     except Exception as e:
-        logging.error(f"Failed to connect to MongoDB: {e}")
-        raise ValueError(f"Invalid URI: {mongo_uri}")
+        logging.error("Failed to connect to MongoDB: %s", e)
+        raise ValueError(f"Invalid URI: {mongo_uri}") from e
 
 
 def get_mongo_database(db_name):
@@ -35,20 +32,16 @@ def get_mongo_database(db_name):
 def get_mongo_collection(db_name, collection_name):
     """
     Returns a MongoDB collection object.
-    :param:
-        db_name (str): Name of the database.
-        collection_name (str): Name of the collection.
-        mongo_uri (str): URI of the MongoDB server. Default is 'mongodb://localhost:27017'.
-    :return:
-        Collection: The requested MongoDB collection object.
-    :raise:
-        ConnectionError: If the connection to MongoDB fails.
+    :param: db_name (str): Name of the database.
+    :param: collection_name (str): Name of the collection.
+    :return: Collection: The requested MongoDB collection object.
+    :raise: ConnectionError: If the connection to MongoDB fails.
     """
     try:
-        client = MongoClient()  # Connect to the MongoDB server
-        database = client[db_name]  # Access the database
-        collection = database[collection_name]  # Access the collection
+        client = MongoClient()
+        database = client[db_name]
+        collection = database[collection_name]
 
         return collection
     except Exception as e:
-        raise ConnectionError(f"Failed to connect to MongoDB: {e}")
+        raise ConnectionError(f"Failed to connect to MongoDB: {e}") from e
