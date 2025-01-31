d9adeba99315cb22f77c548826e9d0e1da950287
mriusero <160402415+mriusero@users.noreply.github.com>
2025-01-31 15:05:01 +0100

    feat: features engineering

--- src/backend/ml/features/features_engineering.py
+++ src/backend/ml/features/features_engineering.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from typing import Dict
 
+
+from ..helper.splitting import split_dataframe
 
 def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
     """Normalize column names to snake_case and remove parentheses."""
     df.columns = (
-        df.columns.str.strip()
-        .str.lower()
-        .str.replace(r'[\s\-_/.]+', '_', regex=True)
-        .str.replace(r'[()]+', '', regex=True)
-        .str.strip('_')
+        df.columns.str.strip()                              # Delete leading/trailing spaces
+        .str.lower()                                        # Convert to lowercase
+        .str.replace(r'[\s\-_/.]+', '_', regex=True)        # Replace spaces, dashes, underscores, dots, and slashes with underscores
+        .str.replace(r'[()]+', '', regex=True)              # Delete parentheses
+        .str.strip('_')                                     # Delete leading/trailing underscores
     )
     return df
 
@@ -44,7 +45,6 @@ def aggregate_users(df: pd.DataFrame) -> pd.DataFrame:
     return df.drop(columns=[f'{col}_norm' for col in ['net_flow_eth', 'total_received_eth', 'total_sent_eth']])
 
 
-
 def aggregation_metrics(df, total_days, group_col, prefix):
     """ Aggregation metrics calculation for transaction"""
     agg_df = df.groupby(group_col).agg(
@@ -71,7 +71,7 @@ def aggregation_metrics(df, total_days, group_col, prefix):
         'count': f'peak_count_{prefix}'
     })[[group_col, f'peak_hour_{prefix}', f'peak_count_{prefix}']]
 
-    agg_df = agg_df.merge(peak, on=group_col, how='left')                               # Fusion et calcul de la fréquence
+    agg_df = agg_df.merge(peak, on=group_col, how='left')                               # Merge peak hour and count
     agg_df[f'tx_frequency_{prefix}'] = agg_df[f'date_nunique_{prefix}'] / total_days
     agg_df.drop(columns=[f'date_nunique_{prefix}'], inplace=True)
     return agg_df
@@ -81,21 +81,21 @@ def aggregate_transactions(users: pd.DataFrame, transactions: pd.DataFrame) -> p
     """Aggregate and enrich users with transactions metrics."""
     with st.spinner("Transactions aggregation..."):
         if transactions.empty:
-            return users            # Gérer le cas des transactions vides si nécessaire
+            return users            # Make sure to return the users DataFrame if no transactions are available
 
-        transactions['timestamp'] = pd.to_datetime(transactions['timestamp'])               # Préparation des données en une seule passe
+        transactions['timestamp'] = pd.to_datetime(transactions['timestamp'])               # Preprocessing
         transactions['date'] = transactions['timestamp'].dt.date
         transactions['hour'] = transactions['timestamp'].dt.hour
         transactions['gas_efficiency'] = transactions['value_eth'] / transactions['gas_used']
 
-        max_ts = transactions['timestamp'].max()            # Calcul des dates globales une seule fois
+        max_ts = transactions['timestamp'].max()            # Calculating total days
         min_ts = transactions['timestamp'].min()
-        total_days = (max_ts - min_ts).days or 1            # Éviter la division par zéro
+        total_days = (max_ts - min_ts).days or 1            # Avoid division by zero
 
-        tx_sent_agg = aggregation_metrics(transactions, total_days, 'from', 'sent')           # Calcul des agrégations pour les transactions envoyées/reçues
-        tx_received_agg = aggregation_metrics(transactions, total_days,'to', 'received')
+        tx_sent_agg = aggregation_metrics(transactions, total_days, 'from', 'sent')           # Calculate metrics for sent transactions
+        tx_received_agg = aggregation_metrics(transactions, total_days,'to', 'received')      # Calculate metrics for received transactions
 
-        merged_df = (           # Fusion finale avec les utilisateurs
+        merged_df = (           # Merge aggregated data with users
             users
             .merge(tx_sent_agg, left_on='address', right_on='from', how='left')
             .merge(tx_received_agg, left_on='address', right_on='to', how='left')
@@ -105,23 +105,57 @@ def aggregate_transactions(users: pd.DataFrame, transactions: pd.DataFrame) -> p
 
 def aggregate_market(merged_df: pd.DataFrame, market: pd.DataFrame) -> pd.DataFrame:
     """Aggregate and enrich users with market metrics"""
+    market['protocol_name'] = (
+        market['protocol_name']
+        .str.strip()
+        .str.lower()
+        .str.replace(r'[\s\-_/.]+', '_', regex=True)
+        .str.replace(r'[()]+', '', regex=True)
+        .str.strip('_')
+    )
 
-    market_protocol_stats = market.groupby('protocol_name').agg({        # Étape 1: Agréger les données de marché par protocole
+    market_protocol_stats = market.groupby('protocol_name').agg({
         'volume': ['mean', 'std'],
         'close_usd': ['mean', 'std'],
         'avg_gas_used_24h': 'mean',
-        'error_rate_24h': 'mean'
+        'error_rate_24h': 'mean',
+        'total_value_eth_24h': 'mean',
+        'nb_tx_24h': 'mean',
+        'nb_unique_receivers_24h': 'mean',
+        'nb_unique_senders_24h': 'mean',
+        'std_gas_used_24h': 'mean',
+        'std_value_eth_24h': 'mean',
+        'max_value_eth_24h': 'mean'
     }).reset_index()
+
     market_protocol_stats.columns = [
         'protocol_name',
         'avg_volume', 'std_volume',
         'avg_close_price', 'std_close_price',
-        'avg_gas_used', 'avg_error_rate'
+        'avg_gas_used', 'avg_error_rate',
+        'avg_total_value_eth', 'avg_nb_tx',
+        'avg_nb_unique_receivers', 'avg_nb_unique_senders',
+        'std_gas_used', 'std_value_eth',
+        'max_value_eth'
     ]
-    protocols = ['curve_dao', 'aave', 'tether', 'uniswap', 'maker', 'yearn_finance', 'usdc', 'dai', 'balancer', 'harvest_finance', 'nftfi']  # Étape 2: Calculer l'exposition des utilisateurs
-    exposure_metrics = ['total_volume_exposure', 'total_volatility_exposure', 'total_gas_exposure',  'total_error_exposure']
 
-    for metric in exposure_metrics:
+    protocols = ['curve_dao', 'aave', 'tether', 'uniswap', 'maker', 'yearn_finance', 'usdc', 'dai', 'balancer',
+                 'harvest_finance', 'nftfi']
+
+    exposure_metrics = {
+        'total_volume_exposure': 'avg_volume',
+        'total_volatility_exposure': 'std_close_price',
+        'total_gas_exposure': 'avg_gas_used',
+        'total_error_exposure': 'avg_error_rate',
+        'total_liquidity_exposure': 'avg_total_value_eth',
+        'total_activity_exposure': 'avg_nb_tx',
+        'total_user_adoption_exposure': ['avg_nb_unique_receivers', 'avg_nb_unique_senders'],
+        'total_gas_volatility_exposure': 'std_gas_used',
+        'total_error_volatility_exposure': 'std_value_eth',
+        'total_high_value_exposure': 'max_value_eth'
+    }
+
+    for metric in exposure_metrics.keys():
         merged_df[metric] = 0.0
 
     for protocol in protocols:
@@ -129,64 +163,47 @@ def aggregate_market(merged_df: pd.DataFrame, market: pd.DataFrame) -> pd.DataFr
         stats = market_protocol_stats[market_protocol_stats['protocol_name'] == protocol]
 
         if not stats.empty:
-            merged_df['total_volume_exposure'] += merged_df[count_col] * stats['avg_volume'].values[0]
-            merged_df['total_volatility_exposure'] += merged_df[count_col] * stats['std_close_price'].values[0]
-            merged_df['total_gas_exposure'] += merged_df[count_col] * stats['avg_gas_used'].values[0]
-            merged_df['total_error_exposure'] += merged_df[count_col] * stats['avg_error_rate'].values[0]
+            for metric, market_col in exposure_metrics.items():
+                if isinstance(market_col, list):
+                    merged_df[metric] += merged_df[count_col] * sum(stats[col].values[0] for col in market_col)
+                else:
+                    merged_df[metric] += merged_df[count_col] * stats[market_col].values[0]
 
-  # eth_price_avg = market[market['symbol'] == 'ETH']['close (usd)'].mean()         # Étape 3: Ajouter des métriques globales
-  # merged_df['global_avg_eth_price'] = eth_price_avg
     return merged_df
-
-
-def split_dataframe(df: pd.DataFrame, train_size: float = 0.7, validation_size: float = 0.15, random_state: int = None) -> Dict[str, pd.DataFrame]:
-    """Split DataFrame into train/validation/test sets without stratification."""
-    if not abs((train_size + validation_size) - 0.85) < 1e-6:
-        raise ValueError("Train + validation sizes must sum to 0.85 (test size fixed at 0.15)")
-    train_df, temp_df = train_test_split(
-        df, train_size=train_size, random_state=random_state
-    )
-    val_test_ratio = validation_size / (1 - train_size)
-    val_df, test_df = train_test_split(
-        temp_df, train_size=val_test_ratio, random_state=random_state
-    )
-    return {
-        'train': train_df,
-        'validation': val_df,
-        'test': test_df
-    }
 
 
 def implement_features() -> None:
     """Main processing pipeline for feature engineering and data splitting."""
+
     if 'dataframes' not in st.session_state:
         raise ValueError("No data loaded in session state")
-
     progress_bar = st.progress(0)
 
-    users = clean_column_names(st.session_state['dataframes']['users'])
+    users = clean_column_names(st.session_state['dataframes']['users'])                 # 1 - Users aggregation
     users = aggregate_users(users)
-    progress_bar.progress(16)
+    progress_bar.progress(20)
 
-    transactions = clean_column_names(st.session_state['dataframes']['transactions'])
+    transactions = clean_column_names(st.session_state['dataframes']['transactions'])   # 2 - Transactions aggregation
     merged_df = aggregate_transactions(users, transactions)
-    progress_bar.progress(48)
+    progress_bar.progress(40)
 
-    market = clean_column_names(st.session_state['dataframes']['market'])
+    market = clean_column_names(st.session_state['dataframes']['market'])               # 3 - Market aggregation
     merged_df = aggregate_market(merged_df, market)
-    progress_bar.progress(64)
+    progress_bar.progress(60)
 
-    merged_df.drop(columns=['from', 'to', 'transactions'], inplace=True)
-    merged_df.fillna(0)
-
-    st.session_state['features'] = split_dataframe(
-        merged_df,
-        random_state=42
-    )
+    merged_df.drop(columns=['from', 'to', 'transactions'], inplace=True)                # 4 - Final cleaning
+    merged_df = merged_df.map(lambda x: pd.NA if x is None else x)
+    merged_df = merged_df.fillna(0)
     progress_bar.progress(80)
 
-    with st.spinner("Saving results..."):
-        merged_df.to_parquet('data/features/features.parquet')
-        progress_bar.progress(100)
+    dataset = split_dataframe(merged_df)                                    # 5 - Split
+    base_path = 'data/features/'
+
+    with st.spinner("Saving features..."):                                  # 6 - Save
+        dataset['train'].to_parquet(f'{base_path}' + 'train.parquet')
+        dataset['validation'].to_parquet(f'{base_path}' + 'validation.parquet')
+        dataset['test'].to_parquet(f'{base_path}' + 'test.parquet')
+
+    progress_bar.progress(100)
 
     st.success('Features processed successfully!')
--- src/frontend/layouts/page2.py
+++ src/frontend/layouts/page2.py
@@ -1,27 +1,98 @@
 import pandas as pd
 import streamlit as st
 
-from src.backend.ml.features.preprocessing import implement_features
+from src.backend.ml.features.features_engineering import implement_features
+from src.backend.core.visualise import DataVisualizer
 
 def page_2():
     st.markdown(
         '<div class="header">#2 Feature Engineering_</div>', unsafe_allow_html=True
     )
+    st.write("""
+    #### 1. General Information
+    - `address`: Unique address of the user on the blockchain.
+
+    #### 2. Transaction Activity
+    - `received_count`: Total number of transactions received.
+    - `total_received_eth`: Total amount of ETH received.
+    - `sent_count`: Total number of transactions sent.
+    - `total_sent_eth`: Total amount of ETH sent.
+
+    #### 3. Types of Interaction with Protocols
+    - `type_dex`: Indicates whether the user interacts with DEXs (Decentralized Exchanges).
+    - `type_lending`: Indicates whether the user interacts with lending protocols.
+    - `type_stablecoin`: Indicates whether the user interacts with stablecoins.
+    - `type_yield_farming`: Indicates whether the user participates in yield farming.
+    - `type_nft_fi`: Indicates whether the user interacts with NFT-Fi protocols.
+
+    #### 4. Engagement with Specific Protocols
+    (Number of transactions made on each protocol)
+    - `curve_dao_count`
+    - `aave_count`
+    - `tether_count`
+    - `uniswap_count`
+    - `maker_count`
+    - `yearn_finance_count`
+    - `usdc_count`
+    - `dai_count`
+    - `balancer_count`
+    - `harvest_finance_count`
+    - `nftfi_count`
+
+    #### 5. User Diversity and Influence
+    - `protocol_type_diversity`: Number of different protocol types used by the user.
+    - `protocol_name_diversity`: Number of unique protocols used by the user.
+    - `net_flow_eth`: Difference between ETH sent and ETH received.
+    - `whale_score`: A score indicating whether the user is a large fund holder.
+
+    #### 6. Sent Transaction Statistics
+    (Minimum, average, median, maximum values, and standard deviations)
+    - `min_sent_eth`, `avg_sent_eth`, `med_sent_eth`, `max_sent_eth`, `std_sent_eth`: Statistics on amounts sent in ETH.
+    - `min_sent_gas`, `avg_sent_gas`, `med_sent_gas`, `max_sent_gas`, `std_sent_gas`: Statistics on gas used for sent transactions.
+    - `avg_gas_efficiency_sent`: Average gas efficiency for sent transactions.
+    - `peak_hour_sent`: Time of day when the user sends the most transactions.
+    - `peak_count_sent`: Maximum number of transactions sent during a given hour.
+    - `tx_frequency_sent`: Average frequency of sent transactions.
+
+    #### 7. Received Transaction Statistics
+    (Same structure as for sent transactions)
+    - `min_received_eth`, `avg_received_eth`, `med_received_eth`, `max_received_eth`, `std_received_eth`: Statistics on amounts received in ETH.
+    - `min_received_gas`, `avg_received_gas`, `med_received_gas`, `max_received_gas`, `std_received_gas`: Statistics on gas used for received transactions.
+    - `avg_gas_efficiency_received`: Average gas efficiency for received transactions.
+    - `peak_hour_received`: Time of day when the user receives the most transactions.
+    - `peak_count_received`: Maximum number of transactions received during a given hour.
+    - `tx_frequency_received`: Average frequency of received transactions.
+
+    #### 8. Exposure to Market Protocols
+    (Evaluation of the user's risk and influence based on the market)
+    - `total_volume_exposure`: Total exposure to the transaction volume of protocols.
+    - `total_volatility_exposure`: Exposure to price volatility of protocols.
+    - `total_gas_exposure`: Exposure to the average gas costs on used protocols.
+    - `total_error_exposure`: Exposure to transaction errors of protocols.
+    - `total_liquidity_exposure`: Exposure to protocol liquidity.
+    - `total_activity_exposure`: Exposure to global transaction activity of protocols.
+    - `total_user_adoption_exposure`: Exposure to the number of active users on protocols.
+    - `total_gas_volatility_exposure`: Exposure to gas volatility used on protocols.
+    - `total_error_volatility_exposure`: Exposure to the variability of transaction errors.
+    - `total_high_value_exposure`: Exposure to high-value transactions on protocols.
+        """)
 
     if st.button("Launch feature engineering"):
         implement_features()
 
     if st.session_state.get('features') is None:
-        features = pd.read_parquet('data/features.parquet', engine='pyarrow')
+        features = pd.read_parquet('data/features/train.parquet', engine='pyarrow')
         st.session_state['features'] = features
 
     features = st.session_state['features']
 
-    train_df = features['train']
+    viz = DataVisualizer(features)
 
     col1, col2 = st.columns([1, 2])
     with col1:
-        st.write('Train set columns:', train_df.columns.tolist())
+        st.write('Train set columns:', features.columns.tolist())
+        viz.missing_data()
     with col2:
-        st.write('Train set :', train_df.sample(10000))
-        st.write('Train set description :', train_df.describe())
+        st.write('Train set :', features.sample(10000))
+        st.write('Train set description :', features.describe())
+        viz.plot_correlation_heatmap()
