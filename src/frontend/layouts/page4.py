011d0cc8f3fe75451d3b5bb96fcafbcae2ddbd55
mriusero <160402415+mriusero@users.noreply.github.com>
2025-02-03 15:21:22 +0100

    refacotr: clean streamlit app

--- src/frontend/layouts/page4.py
+++ src/frontend/layouts/page4.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-from src.backend.ml.pipeline import ml_pipeline
 
 def page_4():
-    st.markdown('<div class="header">#4 ML Pipeline_</div>', unsafe_allow_html=True)
-
-    if st.button("Run ML Pipeline"):
-        ml_pipeline()
+    st.markdown('<div class="header">Summary of Results_</div>', unsafe_allow_html=True)
