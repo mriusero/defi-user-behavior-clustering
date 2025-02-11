871e934d40cf6441b2c24b35409fe6d367fc5895
mriusero <160402415+mriusero@users.noreply.github.com>
2025-02-11 09:26:35 +0100

    fix: unused code for pylint

--- src/backend/core/utils.py
+++ src/backend/core/utils.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import streamlit as st
-import kagglehub
+
 
 
 @st.cache_data
@@ -13,41 +12,42 @@ def data_loading(
     :param base_path: the base path to the files (str)
     :return: a dictionary of dataframes loaded from kaggle and stored in session state (dict)
     """
-    if "dataframes" not in st.session_state:
-        dataframes = {}
-        progress_bar = st.progress(0)
-        total_files = len(files_list)
-
-        for index, file in enumerate(files_list):
-            if file == "users":
-                path = "processed/users_processed.parquet"
-            else:
-                path = "dataset/data/" + file + ".parquet"
-
-            endpoint = kagglehub.dataset_download(base_path, path=path)
-            df = pd.read_parquet(endpoint, engine="pyarrow")
-            dataframes[file] = df
-            progress_bar.progress((index + 1) / total_files)
-
-        ## ====== Data cast ====== ##
-        for _, df in dataframes.items():
-            ## ====== Timestamp conversion ====== ##
-            if "timestamp" in df.columns:
-                df["timestamp"] = pd.to_datetime(
-                    df["timestamp"], errors="coerce", unit="s"
-                )
-
-            elif "first_seen" in df.columns:
-                df["first_seen"] = pd.to_datetime(df["first_seen"], errors="coerce")
-
-            elif "last_seen" in df.columns:
-                df["last_seen"] = pd.to_datetime(df["last_seen"], errors="coerce")
-
-            ## ====== Address conversion ====== ##
-            elif "contract_address" in df.columns:
-                df["contract_address"] = df["contract_address"].astype(str)
-
-            elif "address" in df.columns:
-                df["address"] = df["address"].astype(str)
-
-            st.session_state["dataframes"] = dataframes
+    print("Loading data...", files_list, base_path)
+    #if "dataframes" not in st.session_state:
+    #    dataframes = {}
+    #    progress_bar = st.progress(0)
+    #    total_files = len(files_list)
+#
+    #    for index, file in enumerate(files_list):
+    #        if file == "users":
+    #            path = "processed/users_processed.parquet"
+    #        else:
+    #            path = "dataset/data/" + file + ".parquet"
+#
+    #        endpoint = kagglehub.dataset_download(base_path, path=path)
+    #        df = pd.read_parquet(endpoint, engine="pyarrow")
+    #        dataframes[file] = df
+    #        progress_bar.progress((index + 1) / total_files)
+#
+    #    ## ====== Data cast ====== ##
+    #    for _, df in dataframes.items():
+    #        ## ====== Timestamp conversion ====== ##
+    #        if "timestamp" in df.columns:
+    #            df["timestamp"] = pd.to_datetime(
+    #                df["timestamp"], errors="coerce", unit="s"
+    #            )
+#
+    #        elif "first_seen" in df.columns:
+    #            df["first_seen"] = pd.to_datetime(df["first_seen"], errors="coerce")
+#
+    #        elif "last_seen" in df.columns:
+    #            df["last_seen"] = pd.to_datetime(df["last_seen"], errors="coerce")
+#
+    #        ## ====== Address conversion ====== ##
+    #        elif "contract_address" in df.columns:
+    #            df["contract_address"] = df["contract_address"].astype(str)
+#
+    #        elif "address" in df.columns:
+    #            df["address"] = df["address"].astype(str)
+#
+    #        st.session_state["dataframes"] = dataframes
--- src/frontend/layouts/page1.py
+++ src/frontend/layouts/page1.py
@@ -8,7 +8,7 @@ def page_1():
     st.write("> These graphs only contains 1 000 000 users against 6 870 000 in the real dataset.")
 
     st.write("### Address Protocol Network")
-    st.image("docs/graphics/address_protocol/address_protocol_nx_plot_0.png", caption="")
+    st.image("docs/graphics/address_protocol/address_protocol_nx_plot_2.png", caption="")
 
     st.write("### Address Protocol Network")
     st.image("docs/graphics/address_protocol/address_protocol_nx_plot_1.png", caption="")
