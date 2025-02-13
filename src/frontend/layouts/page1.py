ad12247d136f806c5ac31c6f8c1936bf299ca239
mriusero <160402415+mriusero@users.noreply.github.com>
2025-02-13 19:28:18 +0100

    feat: fix evolved links + add new graphs

--- src/frontend/layouts/page1.py
+++ src/frontend/layouts/page1.py
@@ -7,8 +7,19 @@ def page_1():
     )
     st.write("> These graphs only contains 1 000 000 users against 6 870 000 in the real dataset.")
 
-    st.write("### Address Protocol Network")
-    st.image("docs/graphics/address_protocol/address_protocol_nx_plot_2.png", caption="")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("### Protocols Network")
+        st.image("docs/graphics/address_protocol/address_protocol_nx_plot.png", caption="")
 
-    st.write("### Address Protocol Network")
-    st.image("docs/graphics/address_protocol/address_protocol_nx_plot_1.png", caption="")
+        st.write("### Transactions Numbers *(standardised)*")
+        st.image("docs/graphics/exploratory_analysis/transactions_numbers.png", caption="")
+    with col2:
+        st.write("### Protocol Types Network")
+        st.image("docs/graphics/address_protocol/address_protocol_type_nx_plot.png", caption="")
+
+        st.write("### Transactions Values *(standardised)*")
+        st.image("docs/graphics/exploratory_analysis/transactions_values.png", caption="")
+
+    st.write("### Pairplot (PCA reduced features)")
+    st.image("docs/graphics/exploratory_analysis/pairplot_pca.png", caption="")
